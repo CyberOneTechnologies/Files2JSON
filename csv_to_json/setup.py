8e41a114d56fdb1db52f0f78de9354c7bdb33245
Aarsyth <39145409+tobyarnett@users.noreply.github.com>
2023-06-13 22:41:04 -0500

    Update setup.py

--- csv_to_json/setup.py
+++ csv_to_json/setup.py
@@ -1,7 +1,7 @@
 import os
 
 # Install dependencies from requirements.txt
-os.system('pip install -r csv_to_json/requirements.txt')
+os.system('pip install -r requirements.txt')
 
 # Create the necessary directory structure
 os.makedirs('csv_to_json/input_files', exist_ok=True)
